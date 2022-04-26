d89defa407a83ac1513c2326f72928263baecaa5
Suyeta Saha <ssuyetas@gmail.com>
2022-04-26 23:55:27 +0530

    my first commit

---
+++ src/main/java/com/example/demo/GradleHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleHelloWorldApplication.class, args);
+	}
+
+}
